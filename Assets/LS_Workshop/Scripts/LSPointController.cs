7e4bcf7c7acb37c083d1cbc7e51f379e45dc8def
Richard Hackathorn <richardh@bolder.com>
2020-08-07 07:52:13 -0600

    OVR Gaze, UI panel, useless spotlight
    OVR Gaze is flaky. Need to debug interactions and/or use hand ray for UI.

--- Assets/LS_Workshop/Scripts/LSPointController.cs
+++ Assets/LS_Workshop/Scripts/LSPointController.cs
@@ -8,79 +8,68 @@ public class LSPointController : MonoBehaviour
     // LSpaceController script in prefab LSpaceWorkshop
     // public variables below are set in LSpaceController script when this point is instantiated
     public GameObject ImagePrefab;
-    public string myLSPointName;
-    public float[] myLSPointPos;
-    public float[] myLSPointStd;
-    public Sprite myLSPointSprite;
-    public int myDimSize;
+    public string _LSPointName;
+    public float[] _LSPointPos;
+    public float[] _LSPointStd;
+    public Sprite _LSPointSprite;
+    public int _DimSize;
 
     // private variables below are set in RefreshPoints below during Update
-    private float myPlotScale;
-    private float myPointScale;
-    private int myBaseX;
-    private int myBaseZ;
-    private int myVertY;
+    private float _PlotScale;
+    private float _PointScale;
+    private int _BaseX;
+    private int _BaseZ;
+    private int _VertY;
+    private bool _Variance; 
 
 
-    // Start is called before the first frame update
+    // Register listener for onPlotChange and do initial RefreshPoints
     void Start() 
     {
-        InitializeParameters();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        LSpaceController.onPlotChange += RefreshPoints;
         RefreshPoints();
     }
 
-    private void InitializeParameters()
-    {
-        // Debug.Log("Setting initial Point Parameters..........");
-        // find public parameters in LSpaceController
-        GameObject myGO = GameObject.Find("LSpaceWorkshop");
-        LSpaceController myScript = myGO.GetComponent<LSpaceController>();
+    // Remove listener from onPlotChange when point is destroyed
 
-        // set my parameters for this point
-        myPlotScale = myScript.PlotScale;
-        myPointScale = myScript.PointScale;
-        myBaseX = myScript.BaseX;
-        myVertY = myScript.VertY;
-        myBaseZ = myScript.BaseZ;
-
-        // Debug.Log("transform.name is " + transform.name + " of type " + transform.name.GetType());
-
-        if (transform.name == "0")
-        {
-            Debug.Log("name = " + transform.name);
-            Debug.Log("myPointScale = " + myPlotScale);
-            Debug.Log("myPointScale = " + myPointScale);
-            Debug.Log("myBases = " + myBaseX + " " + myBaseZ + " " + myVertY);
-            Debug.Log("myLSPointPos = " + myLSPointPos[0] + " " + myLSPointPos[1] + " " + myLSPointPos[2]);
-        }
-
+    private void OnDisable() {
+        LSpaceController.onPlotChange -= RefreshPoints;
     }
+
+    // Refresh this point by getting current plot parms and setting pos & scale
+
     private void RefreshPoints()
     {
-        // Debug.Log("Refreshing this point with revised parameters..........");
+        // find public parameters in LSpaceController
+        GameObject _go = GameObject.Find("LSpaceWorkshop");
+        LSpaceController _scr = _go.GetComponent<LSpaceController>();
+
+        // set _ parameters for this point
+        _PlotScale = _scr.PlotScale;
+        Debug.Log("REFRESH--PlotScale = " + _scr.PlotScale);
+        _PointScale = _scr.PointScale;
+        _BaseX = _scr.BaseX;
+        _VertY = _scr.VertY;
+        _BaseZ = _scr.BaseZ;
+        _Variance = _scr.Variance;
 
         // calculate new position on this point
-        float xPos = Convert.ToSingle(myLSPointPos[myBaseX]);
-        float yPos = Convert.ToSingle(myLSPointPos[myVertY]);
-        float zPos = Convert.ToSingle(myLSPointPos[myBaseZ]);
-        Vector3 position = new Vector3 (xPos, yPos, zPos) * myPlotScale;
-        // Debug.Log(position);
+        float xPos = Convert.ToSingle(_LSPointPos[_BaseX]);
+        float yPos = Convert.ToSingle(_LSPointPos[_VertY]);
+        float zPos = Convert.ToSingle(_LSPointPos[_BaseZ]);
+
+        transform.localPosition = new Vector3 (xPos, yPos, zPos) * _PlotScale; //rescale to WorldSpace
 
         // calculate new scale for this point
-        float xStd = Convert.ToSingle(myLSPointStd[myBaseX]);
-        float yStd = Convert.ToSingle(myLSPointStd[myVertY]);
-        float zStd = Convert.ToSingle(myLSPointStd[myBaseZ]);
-        Vector3 scale = new Vector3 (xStd, yStd, zStd) * myPlotScale * myPointScale;
-        
-        // Position point at relative to parent PointHolder
-        transform.localPosition = position;
-        // Scale point relative to Std of LSparent PointHolder
-        transform.localScale = scale;
+        Vector3 scale = new Vector3(1,1,1) * _PointScale;
+        if (_Variance) 
+        {
+            float xStd = Convert.ToSingle(_LSPointStd[_BaseX]) * _PlotScale; //rescale to WorldSpace
+            float yStd = Convert.ToSingle(_LSPointStd[_VertY]) * _PlotScale;
+            float zStd = Convert.ToSingle(_LSPointStd[_BaseZ]) * _PlotScale;
+            scale = new Vector3 (xStd, yStd, zStd) * _PointScale;
+        }
+        transform.localScale = scale; 
 
         // Sets color according to x/y/z value
         GetComponent<Renderer>().material.color = new Color(xPos, yPos, zPos, 1.0f);
